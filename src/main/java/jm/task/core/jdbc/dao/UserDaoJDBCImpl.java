4b841e0af4609e58323e98554b750d2520073b5a
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,154 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    //declare singleton UserDao
+    private static UserDao INSTANCE = null;
+
+    //ddl
+    private static final String CREATE_SQL = """
+            CREATE TABLE IF NOT EXISTS users(
+                id BIGINT UNSIGNED
+                AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                name VARCHAR(100) NULL,
+                lastname VARCHAR(100) NULL,
+                age TINYINT NULL)
+                CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+            """;
+
+    //ddl
+    private static final String DROP_SQL = """
+            DROP TABLE IF EXISTS users;
+            """;
+
+    //dml
+    private static final String SAVE_SQL = """
+            INSERT INTO users(name, 
+                              lastname, 
+                              age) 
+            VALUES (?, ?, ?);
+            """;
+
+    //dml
+    private static final String DELETE_SQL = """
+            DELETE FROM users
+            WHERE id = ?
+            """;
+    // dml
+    private static final String FIND_ALL_SQL = """
+                    SELECT id,
+                           name,
+                           lastname,
+                           age
+                    FROM users
+            """;
+    // dml
+    private static final String TRUNCATE_SQL = """
+            TRUNCATE users
+    """;
+
+    // for singleton make private
+//    public UserDaoJDBCImpl() {
+//    }
+    private UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (var connection = Util.getConnection();
+             var statement = connection.createStatement();) {
+            statement.execute(CREATE_SQL);
+        } catch (SQLException throwable) {
+            System.out.println("SQLException: " + throwable.getMessage());
+        }
     }
 
     public void dropUsersTable() {
-
+        try (var connection = Util.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(DROP_SQL);
+        } catch (SQLException throwable) {
+            System.out.println("SQLException: " + throwable.getMessage());
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (var connection = Util.getConnection();
+             var preparedStatement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) {
 
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwable) {
+            throw new DaoException(throwable);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (var connection = Util.getConnection();
+        var preparedStatement = connection.prepareStatement(DELETE_SQL)){
+            preparedStatement.setLong(1, id);
+        } catch (SQLException throwable) {
+            throw new RuntimeException(throwable);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        try (var connection = Util.getConnection();
+             var preparedStatement = connection.prepareStatement(FIND_ALL_SQL)) {
+            var resultSet = preparedStatement.executeQuery();
+
+            List<User> users = new ArrayList<>();
+            while (resultSet.next()) {
+                users.add(buildUser(resultSet));
+            }
+            return users;
+
+        } catch (SQLException throwable) {
+            throw new DaoException(throwable);
+        }
+    }
+
+    private static User buildUser(ResultSet resultSet) throws SQLException {
+        User user = new User(
+                resultSet.getString("name"),
+                resultSet.getString("lastname"),
+                resultSet.getByte("age")
+        );
+        user.setId(resultSet.getLong("id"));
+        return user;
     }
 
     public void cleanUsersTable() {
+        try (var connection = Util.getConnection();
+            var statement = connection.createStatement()) {
+            statement.execute(TRUNCATE_SQL);
+        } catch (Exception throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
 
+    // create singleton UserDao
+    public static UserDao getInstance() {
+        if (INSTANCE == null) {
+            synchronized (UserDaoJDBCImpl.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new UserDaoJDBCImpl();
+                }
+            }
+        }
+        return INSTANCE;
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,99 @@
+/**
+ * Используется пул соединений, паттерн Connection pool
+ */
+
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // реализуйте настройку соединения с БД
+    private static final String URL = "db.url";
+    private static final String USER = "db.username";
+    private static final String PASSWORD = "db.password";
+    private static final String POOL_SIZE_KEY = "db.pool_size";
+    private static final Integer DEFAULT_POOL_SIZE = 10;
+    private static BlockingQueue<Connection> pool;
+    private static List<Connection> sourceConnections;
+
+    private Util() {
+    }
+
+    static {
+        loadDriver();
+        initConnectionPool();
+    }
+
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL),
+                    PropertiesUtil.get(USER),
+                    PropertiesUtil.get(PASSWORD)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to connect to database", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try{
+            return pool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static void initConnectionPool() {
+        String poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
+        var size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+
+        // инициализируем pool с правильным размером
+        pool = new ArrayBlockingQueue<>(size);
+
+        // инициализируем sourceConnections
+        sourceConnections = new ArrayList<>(size);
+
+        // добавляем все соединения в pool
+        for (int i = 0; i < size; i++) {
+            var connection = open();
+            var proxyConnection = (Connection)
+                    Proxy.newProxyInstance(Util.class.getClassLoader(),
+                            new Class[]{Connection.class},
+                            (proxy, method, args) -> method.getName().equals("close")
+                            ? pool.add((Connection) proxy)
+                            : method.invoke(connection, args));
+            pool.add(proxyConnection);
+            sourceConnections.add(connection);
+        }
+    }
+
+    public static void closePool() {
+        try {
+            for (Connection sourceConnection : sourceConnections) {
+                sourceConnection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static void loadDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
